233927523d196ed7b37e0a04ca7bbda633923b43
Behrad <behradz@gmail.com>
2017-07-05 09:23:22 +0430

    added a containing test for https://github.com/mcollina/aedes-cached-persistence/pull/4

--- abstract.js
+++ abstract.js
@@ -265,6 +265,30 @@ function abstractPersistence (opts) {
     })
   })
 
+  testInstance('get subscriptions list after an unsubscribe', function (t, instance) {
+    var client1 = { id: 'abcde' }
+    var client2 = { id: 'efghi' }
+    var subs = [{
+      topic: 'helloagain',
+      qos: 1
+    }]
+
+    instance.addSubscriptions(client1, subs, function (err) {
+      t.notOk(err, 'no error for client 1')
+      instance.addSubscriptions(client2, subs, function (err) {
+        t.notOk(err, 'no error for client 2')
+        instance.removeSubscriptions(client2, [subs[0].topic], function (err, reClient) {
+          t.notOk(err, 'no error for removeSubscriptions')
+          instance.subscriptionsByTopic(subs[0].topic, function (err, clients) {
+            t.notOk(err, 'no error getting subscriptions by topic')
+            t.deepEqual(clients[0].clientId, client1.id)
+            instance.destroy(t.end.bind(t))
+          })
+        })
+      })
+    })
+  })
+
   testInstance('QoS 0 subscriptions, restored but not matched', function (t, instance) {
     var client = { id: 'abcde' }
     var subs = [{
