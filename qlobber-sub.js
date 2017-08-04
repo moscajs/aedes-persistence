16a5ba9d7b5201c087220a8aaa9f65643473aa54
David Halls <dahalls@gmail.com>
2017-08-04 09:03:56 +0100

    Allow match to filter by specified topic

--- qlobber-sub.js
+++ qlobber-sub.js
@@ -23,14 +23,26 @@ QlobberSub.prototype._add_value = function (clientMap, val) {
   topicMap.set(val.topic, val.qos)
 }
 
-QlobberSub.prototype._add_values = function (dest, clientMap) {
+QlobberSub.prototype._add_values = function (dest, clientMap, topic) {
   for (var clientIdAndTopicMap of clientMap) {
-    for (var topicAndQos of clientIdAndTopicMap[1]) {
-      dest.push({
-        clientId: clientIdAndTopicMap[0],
-        topic: topicAndQos[0],
-        qos: topicAndQos[1]
-      })
+    var clientId = clientIdAndTopicMap[0]
+    var topicMap = clientIdAndTopicMap[1]
+    if (topic === undefined) {
+      for (var topicAndQos of topicMap) {
+        dest.push({
+          clientId: clientId,
+          topic: topicAndQos[0],
+          qos: topicAndQos[1]
+        })
+      }
+    } else {
+      var qos = topicMap.get(topic)
+      if (qos !== undefined) {
+        dest.push({
+          clientId: clientId,
+          qos: qos
+        })
+      }
     }
   }
 }
@@ -51,8 +63,8 @@ QlobberSub.prototype.test_values = function (clientMap, val) {
   return topicMap && topicMap.has(val.topic)
 }
 
-QlobberSub.prototype.match = function (topic) {
-  return this._match([], 0, topic.split(this._separator), this._trie)
+QlobberSub.prototype.match = function (topic, ctx) {
+  return this._match([], 0, topic.split(this._separator), this._trie, ctx)
 }
 
 module.exports = QlobberSub
