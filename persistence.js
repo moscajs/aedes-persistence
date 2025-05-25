3946e919c3cd46f39d8caa244b04878f5d411d10
Hans Klunder <hans.klunder@gmail.com>
2025-05-25 20:23:51 +0200

    fix import/export of Packet

--- persistence.js
+++ persistence.js
@@ -1,6 +1,8 @@
 'use strict'
 
+const Packet = require('aedes-packet')
 const { CallBackPersistence } = require('./callBackPersistence.js')
 const AsyncPersistence = require('./asyncPersistence.js')
 const asyncInstanceFactory = (opts) => new AsyncPersistence(opts)
 module.exports = (opts) => new CallBackPersistence(asyncInstanceFactory, opts)
+module.exports.Packet = Packet
